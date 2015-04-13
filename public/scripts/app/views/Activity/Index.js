7ceeb565e4a1a0f3ffd8f38a43dc2dba9dc0770a
Felix Niklas <mrflix@gmail.com>
2015-04-13 16:56:27 +0200

    add globalListeners method for future hidden views

--- public/scripts/app/views/Activity/Index.js
+++ public/scripts/app/views/Activity/Index.js
@@ -6,12 +6,18 @@ define(function(require) {
       , Base             = require('app/views/Base')
       , ActivityItemView = require('app/views/Activity/ActivityItem')
       , log              = require('bows.min')('Views:Activity:Index')
+    require('jquery.scrollparent')
 
     return Base.extend({
 
       template: _.template(require('text!tpl/Activity/Index.html')),
 
       requiresLogin: true,
+
+      infiniteScrollTriggerPoint: 300, // in pixels from the bottom
+      isInfiniteScrollLoading: false,
+
+      untouched: true,
 
       title: 'My Posts',
 
@@ -23,6 +29,28 @@ define(function(require) {
 
       initialize: function(options) {
         this.options = options
+        this.router = options.router
+
+        // this.collection = new Topics(null, {
+        //   channelJid: this.options.channelJid,
+        //   comparator: false,
+        // })
+        // this.collection.on('loaded:activities', this.addActivityItems, this)
+
+
+        // if (0 !== this.collection.length) {
+        //   return this.addActivityItems(0)
+        // }
+
+        // this.collection.sync()
+      },
+
+      unbindGlobalListeners: function() {
+        this.scrollParent.off('scroll.activityList')
+      },
+
+      reactivateGlobalListeners: function() {
+        this.scrollParent.on('scroll.activityList', this.onScroll)
       },
       
       render: function() {
@@ -30,9 +58,55 @@ define(function(require) {
         return this
       },
 
-      appendPosts: function() {
+      addActivityItems: function(count) {
         // for each post, append post item
+        var newItems = this.collection.models.slice(-length)
+        var fragment = document.createDocumentFragment()
+        var self = this
+
+        for(var i=0, l=newItems.length; i<l; i++){
+          var item = new ActivityItemView({
+            model: newItems[i],
+            router: self.router
+          })
+          fragment.appendChild(item.render().el)
+        }
+        this.$el.append(fragment)
+
+        this.isInfiniteScrollLoading = false
+
+        if (this.untouched) {
+          this.scrollParent = this.$el.scrollParent()
+          this.scrollParent.on('scroll.activityList', this.onScroll)
+          this.untouched = false
+        }
+
+        if (this.collection.allItemsLoaded()) {
+          this.$el.find('.js-infiniteLoader').addClass('is-hidden')
+          this.scrollParent.off('scroll.activityList')
+        }
+
+        var scrollHeight = (this.scrollParent.get(0) == document ? $('body') : this.scrollParent).prop('scrollHeight')
+        this.triggerPos = scrollHeight - this.infiniteScrollTriggerPoint
+        this.height = (this.scrollParent.get(0) == document ? $(window) : this.scrollParent).height()
       },
+
+      onScroll: function() {
+        if(this.isInfiniteScrollLoading) {
+          return
+        }
+
+        var viewBottomEdge = this.scrollParent.scrollTop() + this.height
+
+        if(viewBottomEdge > this.triggerPos) {
+          this.loadMoreItems()
+        }
+      },
+
+      loadMoreItems: function() {
+        this.isInfiniteScrollLoading = true
+        this.collection.sync()
+      }
 
     })
 
