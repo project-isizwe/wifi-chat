601ad74d0c7f704b4e1b8f21d3d5e3df4f2879d9
Felix Niklas <mrflix@gmail.com>
2015-04-14 14:41:33 +0200

    detect banner background

--- public/scripts/app/views/Channel/Header.js
+++ public/scripts/app/views/Channel/Header.js
@@ -18,11 +18,22 @@ define(function(require) {
       },
 
 	    initialize: function(options) {
+        _.bindAll(this, 'render')
+
         this.router = options.router
         this.options = options
-        _.bindAll(this, 'render')
-        this.on('render', this.afterRender, this)
-        this.loadAvatar()
+      },
+
+      render: function() {
+        this.$el.html(this.template(_.extend(this.model.attributes, {
+          avatarUrl: this.avatar && this.avatar.get('url'),
+          bannerBackground: this.avatar && this.detectBackgroundColor()
+        })))
+
+        if(!this.avatar)
+          this.loadAvatar()
+
+        return this
       },
 
       loadAvatar: function() {
@@ -31,14 +42,29 @@ define(function(require) {
           height: 160,
           width: 'auto'
          })
-        this.avatar.once('loaded:avatar', this.render, this)
+        this.avatar.once('loaded:avatar', this.showAvatar, this)
       },
 
-      afterRender: function() {
-        if (this.avatar.get('url')) {
-          this.$el.find('.channel-banner')
-            .css('background-image', 'url("' + this.avatar.get('url') + '")')
-        }          
+      showAvatar: function() {
+        this.$el.find('.channel-banner')
+          .css({
+            backgroundImage: 'url("' + this.avatar.get('url') + '")',
+            backgroundColor: this.detectBackgroundColor()
+          })       
+      },
+
+      detectBackgroundColor: function() {
+        var canvas = document.createElement('canvas')
+        var ctx = canvas.getContext('2d')
+        canvas.width = this.avatar.image.width
+        canvas.height = this.avatar.image.height
+        ctx.drawImage(this.avatar.image, 0, 0)
+        // if it fails, it fails because of cross-origin data. no background color then
+        try {
+          var topLeftPixel = ctx.getImageData(0, 0, 1, 1).data
+          // can't use .join()
+          return 'rgba('+ topLeftPixel[0] +','+ topLeftPixel[1] +','+ topLeftPixel[2] +','+ topLeftPixel[3] +')'
+        } catch(error) {}
       },
 
       showNewTopicScreen: function() {
