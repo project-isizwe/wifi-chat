685ff7f45215d08475761dc32049b2cd7a782f67
Felix Niklas <mrflix@gmail.com>
2015-04-15 12:32:54 +0200

    fix lowercase to respect backspace

--- public/scripts/app/views/Signup.js
+++ public/scripts/app/views/Signup.js
@@ -20,7 +20,8 @@ define(function(require) {
         events: {
           'submit': 'register',
           'blur input': 'inidicateValidation',
-          'keyup input': 'lowerCase'
+          'keyup input[name=username]': 'lowerCase',
+          'paste input[name=username]': 'lowerCase',
         },
 
         inidicateValidation: function(event) {
@@ -75,9 +76,24 @@ define(function(require) {
           })
         
         },
-
+        
         lowerCase: function(event) {
-          $(event.target).val($(event.target).val().toLowerCase())
+          if (this.isCharacterKeyPress(event)) {
+            event.target.value = event.target.value.toLowerCase()
+          }
+        },
+
+        isCharacterKeyPress: function(evt) {
+          if (typeof evt.which == "undefined") {
+            // This is IE, which only fires keypress events for printable keys
+            return true;
+          } else if (typeof evt.which == "number" && evt.which > 0) {
+            // In other browsers except old versions of WebKit, evt.which is
+            // only greater than zero if the keypress is a printable key.
+            // We need to filter out backspace and ctrl/alt/meta key combinations
+            return !evt.ctrlKey && !evt.metaKey && !evt.altKey && evt.which != 8;
+          }
+          return false;
         },
       
         accountCreated: function(model, response) {
