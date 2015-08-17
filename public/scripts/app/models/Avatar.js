0b5c58e73863aeffb634ec2718230bea4995dfed
Retro <retrobunny@evilprofessor.co.uk>
2015-08-17 23:55:49 +0100

    Let's just use old Avatar.js for now

--- public/scripts/app/models/Avatar.js
+++ public/scripts/app/models/Avatar.js
@@ -2,22 +2,42 @@ define(function(require) {
 
     'use strict';
   
-    var log  = require('bows.min')('Models:Avatar')
-      , File = require('app/models/File')
+    var Backbone     = require('backbone')
+      , mediaServers = require('app/store/MediaServers')
+      , MediaServer  = require('app/models/MediaServer')
+      , socket       = require('app/utils/socket')
+      , user         = require('app/store/User')
+      , log          = require('bows.min')('Models:Avatar')
 
-    return File.extend({
+    return Backbone.Model.extend({
 
-      method: 'PUT',
+      defaults: {
+        url: null,
+        cachebust: ''
+      },
+
+      uploadToken: null,
+      complete: false,
+
+      xmppVerifyEvent: 'xmpp.buddycloud.http.verify',
 
       initialize: function() {
-        this.discoverMediaServer(_.bind(this.setAvatar, this))
+        this.attributes.domain = this.attributes.jid.split('@')[1]
+        this.mediaServer = mediaServers.findWhere({ domain: this.get('domain') })
+        if (this.mediaServer) {
+          if (this.mediaServer.get('url')) {
+            return this.setAvatar()
+          }
+        } else {
+          this.mediaServer = new MediaServer({ domain: this.get('domain')})
+          mediaServers.add(this.mediaServer)
+        }
         this.mediaServer.on('change:url', this.setAvatar, this)
       },
 
       setAvatar: function() {
-        /* check if avatar is there
-         * by requesting a default sized one
-         */
+        // check if avatar is there
+        // by requesting a default sized one
         var url = this.getBaseUrl() + '?maxwidth=44&maxheight=44' + this.get('cachebust')
         this.image = new Image()
         this.image.crossOrigin = "Anonymous";
@@ -27,6 +47,10 @@ define(function(require) {
           self.complete = true
         }
         this.image.src = url
+      },
+
+      isLoaded: function() {
+        return this.complete
       },
 
       getBaseUrl: function() {
@@ -56,6 +80,81 @@ define(function(require) {
         return this.getBaseUrl() + '?' + parameters.join('&') + this.get('cachebust')
       },
 
+      verifyFileUpload: function(data) {
+        var event = 'xmpp.buddycloud.http.confirm'
+        if (this.uploadToken !== data.request.id) {
+          event = 'xmpp.buddycloud.http.deny'
+        }
+        this.uploadToken = null
+        socket.send(
+          event,
+          this.getVerificationPayload(data),
+          function(error, data) { log(error, data) }
+        )
+      },
+
+      getVerificationPayload: function(data) {
+        return { 
+          to: data.from.domain,
+          id: data.id,
+          request: data.request,
+          type: data.type
+        }
+      },
+
+      uploadAvatar: function(event) {
+        socket.once(this.xmppVerifyEvent, this.verifyFileUpload, this)
+
+        var formData = new FormData()
+        var file =  event.target.files[0]
+        var reader = new FileReader()
+        var self = this
+
+        reader.onload = function(event) {
+          var fileData = event.target.result
+          formData.append('data', file)
+          formData.append('content-type', file.type)
+          formData.append('filename', file.name)
+
+          var ajaxOpts = {
+            url: self.getBaseUrl(),
+            type: 'PUT',
+            headers: {
+              'Authorization':'Basic ' + self.getAuthorizationToken(),
+            },
+            cache: false,
+            contentType: false,
+            processData: false,
+            success: function(data, status, jqXHR) {
+              log("success", data, status, jqXHR)
+              self.set('cachebust', '&'+ Date.now())
+              self.trigger('change:url', self, self.get('url'))
+            },
+            error: function(jqXHR, status, error) {
+              self.trigger('error:avatar', 'Something went wrong. Please try again later.')
+              log('error', {
+                  status: status,
+                  error: error,
+                  xhr: jqXHR
+              })
+            }
+          }
+          ajaxOpts.data = formData
+          $.ajax(ajaxOpts)
+        }
+        reader.readAsText(file)
+      },
+
+      getAuthorizationToken: function() {
+        var token = user.get('fullJid') + ':' + this.generateUploadToken()
+        return btoa(token)
+      },
+
+      generateUploadToken: function() {
+        this.uploadToken = Math.random().toString(36).substring(7)
+        return this.uploadToken
+      },
+      
     })
 
 })
